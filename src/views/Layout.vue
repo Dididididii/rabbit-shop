1aee4f4901a3370a30e4fffac4571ac65cd91a9f
Dididididii <10611108+dididididii@user.noreply.gitee.com>
2022-07-05 10:24:03 +0800

    底部布局完成

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -10,18 +10,22 @@
     <!-- 二级路由 -->
     <router-view />
  </main>
- <footer>底部</footer>
+ <footer>
+  <appFooterVue />
+ </footer>
  </div>
 </template>
 
 <script>
 import appTopnavVue from '@/components/app-topnav.vue'
 import appHeaderVue from '@/components/app-header.vue'
+import appFooterVue from '@/components/app-footer.vue'
 export default {
   name: 'xtx-layout',
   components: {
     appTopnavVue,
-    appHeaderVue
+    appHeaderVue,
+    appFooterVue
   }
 }
 </script>
