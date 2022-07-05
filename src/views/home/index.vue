66cffbacb87bb20531e1643262b016dbaaabfac4
Dididididii <10611108+dididididii@user.noreply.gitee.com>
2022-07-05 16:34:49 +0800

    左侧分类完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    首页
+  <div class="page-home">
+    <div class="home-entry">
+      <div class="container">
+        <!-- 左侧分类 -->
+        <homeCategoryVue />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import homeCategoryVue from './components/home-category.vue'
 export default {
-  name: 'xtx-home-papg'
+  name: 'xtx-home-papg',
+  components: {
+    homeCategoryVue
+  }
 }
 </script>
 
